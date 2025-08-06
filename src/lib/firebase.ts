dee1c50f66959d86bc2731db7e98a8f25321272c
Saurabh Rajput <rajputsaurabh19990@gmail.com>
2025-08-06 20:23:21 +0000

    // firebase.js import { initializeApp } from "firebase/app"; import { ge

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,18 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./firebase-config";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5j0uZv46j4S_YJpBC5T-dB6Haiw1knZY",
+  authDomain: "dhenuguide.firebaseapp.com",
+  projectId: "dhenuguide",
+  storageBucket: "dhenuguide.appspot.com",
+  messagingSenderId: "103418941595",
+  appId: "1:103418941595:web:2d00b05538618c0327763e",
+  measurementId: "G-05GNDNEQ7J"
+};
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
