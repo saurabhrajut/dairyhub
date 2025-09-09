43a98c0d79c39b3dc77292898b671bc39f3e7d76
Saurabh Rajput <rajputsaurabh19990@gmail.com>
2025-09-09 16:26:27 +0000

    Try fixing this error: `Console Error: [2025-09-09T16:25:01.164Z] @fireb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,18 @@
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage }from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "projectId": "dhenuguide",
-  "appId": "1:103418941595:web:927dc0469211704027763e",
-  "storageBucket": "dhenuguide.firebasestorage.app",
-  "apiKey": "AIzaSyC5j0uZv46j4S_YJpBC5T-dB6Haiw1knZY",
-  "authDomain": "dhenuguide.firebaseapp.com",
-  "measurementId": "G-L27VCQC1C1",
-  "messagingSenderId": "103418941595"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 
