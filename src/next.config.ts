65738a270e20990b60069f150786f4340dcc443a
Saurabh Rajput <rajputsaurabh19990@gmail.com>
2025-09-22 11:52:45 +0000

    abhi abhi open nhi ho rha h app, plz open it properly

--- src/next.config.ts
+++ src/next.config.ts
@@ -29,7 +29,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https//',
+        protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
