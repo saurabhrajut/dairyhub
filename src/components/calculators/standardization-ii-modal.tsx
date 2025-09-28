91c33dba4fb40efd24f804ce6ba34df1f70de453
saurabhrajut <rajputsaurabh19990@gmail.com>
2025-09-28 22:34:24 +0530

    Update standardization-ii-modal.tsx

--- src/components/calculators/standardization-ii-modal.tsx
+++ src/components/calculators/standardization-ii-modal.tsx
@@ -1,7 +1,7 @@
 
 "use client"
 
-import { useState, memo, useCallback, useEffect } from "react"
+import { useState, memo, useCallback, useEffect, useMemo } from "react"
 import {
   Dialog,
   DialogContent,
