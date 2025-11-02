76c552d24f0637b911612ffe58f2c105128c8dea
saurabhrajut <rajputsaurabh19990@gmail.com>
2025-11-02 15:30:15 +0530

    Delete src/app/signup directory

--- src/app/signup/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-// यह SignupPage कंपोनेंट को रेंडर करने के लिए route फाइल है।
-import SignupPage from '@/app/signup/SignupPage';
-
-export default function Page() {
-  return <SignupPage />;
-}
