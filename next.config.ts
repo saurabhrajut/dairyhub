7d5ed634ea26d116f071da6e3c36a9f92f77ce96
saurabhrajut <rajputsaurabh19990@gmail.com>
2025-11-07 07:53:10 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -42,8 +43,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {},
-  serverExternalPackages: ['handlebars', 'dotprompt'],
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    razorpayKeyId: process.env.RAZORPAY_KEY_ID,
+    razorpayKeySecret: process.env.RAZORPAY_KEY_SECRET,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    razorpayKeyId: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+  },
 };
 
 export default nextConfig;
