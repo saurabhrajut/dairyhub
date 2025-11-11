321cf8b83fa5eefdba8a5266e0e4689c194ef944
saurabhrajut <rajputsaurabh19990@gmail.com>
2025-11-12 02:08:21 +0530

    Update standardization-ii-modal.tsx

--- src/components/calculators/standardization-ii-modal.tsx
+++ src/components/calculators/standardization-ii-modal.tsx
@@ -12,6 +12,7 @@ import {
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Checkbox } from "@/components/ui/checkbox"
 import { componentProps } from "@/lib/data"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
