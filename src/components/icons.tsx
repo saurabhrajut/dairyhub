64195027ba32069c6683baf8c9b8a28656872377
saurabhrajut <rajputsaurabh19990@gmail.com>
2025-12-30 17:14:51 +0530

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,3 @@
-
 import type { SVGProps } from "react";
 import { TestTube2, Building2, Combine, Percent } from "lucide-react";
 
@@ -193,10 +192,28 @@ export const MilkCanIcon = (props: SVGProps<SVGSVGElement>) => (
     </svg>
 );
 
+// Added HplcIcon here
+export const HplcIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <rect x="4" y="2" width="16" height="20" rx="2" />
+    <path d="M8 6h8" />
+    <path d="M8 10h8" />
+    <path d="M12 14v4" />
+    <path d="M8 18h8" />
+  </svg>
+);
 
 export const ReagentIcon = TestTube2;
     
-
 export const DepartmentIcon = Building2;
 
 export const Beaker = (props: SVGProps<SVGSVGElement>) => (
@@ -204,8 +221,3 @@ export const Beaker = (props: SVGProps<SVGSVGElement>) => (
         <path d="M4.5 3h15"/><path d="M6 3v16a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V3"/><path d="M6 14h12"/>
     </svg>
 );
-
-
-    
-
-    
