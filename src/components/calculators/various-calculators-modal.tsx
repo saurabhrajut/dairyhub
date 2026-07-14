7261e5b9eb363a0ec4e29c4ae1a2594623be295b
Saurabh kumar <rajputsaurabh19990@gmail.com>
2026-07-15 02:37:55 +0530

    Update various-calculators-modal.tsx

--- src/components/calculators/various-calculators-modal.tsx
+++ src/components/calculators/various-calculators-modal.tsx
@@ -79,7 +79,7 @@ const productSampleWeights: Record<string, string> = {
     other: "10.0"
 };
 
-const LabFormatsCalc = dynamic(() => import("./lab-formats-calc").then(m => m.default), { 
+const LabFormatsCalc = dynamic(() => import("./lab-formats-calc").then(m => ({ default: m.LabFormatsCalc })), { 
   ssr: false, 
   loading: () => <div className="flex items-center justify-center p-8"><div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary"></div></div> 
 });
